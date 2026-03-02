61bb1340e7b78037a45dd6418d4e7b240f5d4ef2
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-03-03 00:18:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,13 @@ import apis.youtube_api as youtube
 # ----- 3. FASTAPI WRAPPER -----
 #server = FastAPI(title="Dash Main App")
 #server = FastAPI(title="Dash Main App", lifespan=lambda app: youtube.mcp.session_manager.run())
-server = FastAPI(title="Dash Main App",lifespan=youtube.lifespan)
+#server = FastAPI(title="Dash Main App",lifespan=youtube.lifespan)
 #server.mount("/youtube", youtube.mcp.http_app(stateless_http=True))
+
+youtube_app = youtube.mcp.http_app(stateless_http=True)
+server = FastAPI(title="Dash Main App", lifespan=youtube_app.lifespan)
+server.mount("/youtube", youtube_app)
+
 
 # ----- 3.1 HEALTH ENDPOINT -----
 @server.get("/health")
@@ -33,7 +38,7 @@ def health():
 #server.mount("/youtube", youtube.mcp.sse_app())
 #server.mount("/youtube", youtube.mcp.streamable_http_app())
 #server.mount("/youtube", youtube.mcp.streamable_http_app(stateless_http=True))
-server.mount("/youtube", youtube.mcp.http_app(stateless_http=True))
+#server.mount("/youtube", youtube.mcp.http_app(stateless_http=True))
 
 server.include_router(bybit.router,         prefix="/api/bybit",         tags=["Bybit"])
 server.include_router(kraken.router,        prefix="/api/kraken",        tags=["Kraken"])
