677fe353da6cfb07a7dbc9810e0f2ec9d8dd3717
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-03-02 22:17:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def health():
 
 # ----- 3.2. API ROUTERS -----
 #server.mount("/youtube", youtube.mcp.sse_app())
-server.mount("/youtube", youtube.mcp.streamable_http_app())
+#server.mount("/youtube", youtube.mcp.streamable_http_app())
+server.mount("/youtube", youtube_mcp.mcp.streamable_http_app(stateless_http=True))
 
 server.include_router(bybit.router,         prefix="/api/bybit",         tags=["Bybit"])
 server.include_router(kraken.router,        prefix="/api/kraken",        tags=["Kraken"])
