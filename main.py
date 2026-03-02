8d5e4ea3fe094cc35225be810f76a3e2429d9f90
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-03-02 18:56:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def health():
 # This creates the /sse and /messages endpoints n8n needs
 #app.mount("/mcp", mcp.sse_app())
 
-server.mount("/youtube_mcp", youtube_mcp.mcp.sse_app())
+#server.mount("/youtube_mcp", youtube_mcp.mcp.sse_app())
+server.mount("/youtube_mcp", youtube_mcp.mcp.streamable_http_app())
 
 #mcp = FastMCP("strava", stateless_http=True)
 #app = FastAPI(title="Strava",lifespan=lambda app: mcp.session_manager.run())
